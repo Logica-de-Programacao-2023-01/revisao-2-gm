b801ecd6b6fdcdfff5a687c42d56d70d698dbdf9
guilemouraoc <86135577+guilemouraoc@users.noreply.github.com>
2023-05-29 12:57:48 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,16 +1,12 @@
 package q1
 
-import (
-	"fmt"
-)
-
 type Student struct {
 	Name     string
 	Age      int
 	Subjects map[string]float64
 }
 
-func mergeStudentData(studentData1, studentData2 map[string]Student) map[string]Student {
+func MergeStudentData(studentData1, studentData2 map[string]Student) map[string]Student {
 	mergedData := make(map[string]Student)
 
 	// Copia as informações do studentData1 para o mapa mergedData
@@ -33,56 +29,4 @@ func mergeStudentData(studentData1, studentData2 map[string]Student) map[string]
 	}
 
 	return mergedData
-}
-
-func main() {
-	studentData1 := map[string]Student{
-		"John": {
-			Name: "John",
-			Age:  20,
-			Subjects: map[string]float64{
-				"Math":   85.0,
-				"Science": 78.0,
-			},
-		},
-		"Alice": {
-			Name: "Alice",
-			Age:  19,
-			Subjects: map[string]float64{
-				"English": 90.0,
-				"Physics": 82.0,
-			},
-		},
-	}
-
-	studentData2 := map[string]Student{
-		"John": {
-			Name: "John",
-			Age:  20,
-			Subjects: map[string]float64{
-				"Math":   90.0,
-				"History": 88.0,
-			},
-		},
-		"Bob": {
-			Name: "Bob",
-			Age:  21,
-			Subjects: map[string]float64{
-				"Chemistry": 79.0,
-				"Physics":   85.0,
-			},
-		},
-	}
-
-	mergedData := mergeStudentData(studentData1, studentData2)
-
-	// Exibe as informações combinadas dos alunos
-	for _, student := range mergedData {
-		fmt.Printf("Name: %s, Age: %d\n", student.Name, student.Age)
-		fmt.Println("Subjects:")
-		for subject, grade := range student.Subjects {
-			fmt.Printf("- %s: %.2f\n", subject, grade)
-		}
-		fmt.Println()
-	}
 }
